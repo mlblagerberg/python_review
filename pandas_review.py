370d3f9f4325d4dbcd5a28cd6af4f83e2bb1d18c
mlblagerberg <mlblagerberg@gmail.com>
2024-01-11 09:16:16 -0800

    Updating name of repo and file that make more sense

---
+++ pandas_review.py
@@ -0,0 +1,11 @@
+"""
+General Pandas review
+January 10th, 2024
+Madeleine Lagerberg
+"""
+
+import pandas as pd
+
+df = pd.read_csv('pokemon_data.csv' ) # df = pd.read_csv('pokemon_data.csv')
+
+print(df.head(10))
