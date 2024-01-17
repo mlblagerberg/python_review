3ea4375239c07cc6e73f154fd91fbec9ed9d4433
mlblagerberg <mlblagerberg@gmail.com>
2024-01-16 16:50:42 -0800

    Worked out first half of problem mapping categories from dictionary to the dataframe

--- interview_recreation.py
+++ interview_recreation.py
@@ -30,7 +30,8 @@ df.loc[df['Total'] < 330,  'total_category'] = 1
 df.loc[(df['Total'] >= 330) & (df['Total'] <  450), 'total_category'] = 2
 # df.loc[(df['Total'] >= 450) & (df['Total'] < 515),  'total_category'] = 3
 # print(df.isna().sum())
-print(df.head())
+# print(df.head())
+# print(type(df['total_category']))
 
 # Iterate over a dictionary
 for i in range(1, len(sample_dictionary) + 1):
@@ -39,12 +40,30 @@ for i in range(1, len(sample_dictionary) + 1):
     ith_key = list_index[i-1]
     print(f'{ith_value} and {ith_key}')
 
+# Shows that the column total category  is type float when we want it to be int
+print(df.info())
+
+
+# df['total_category'] = df['total_category'].astype(int) # won't work because there are nulls
+
 # Combine look up with constructing column based on dictionary iteration
 for i in range(0, len(sample_dictionary)):
     ith_value = sample_dictionary[i+1]
+    # print(type(ith_value))
     list_index = list(sample_dictionary.keys())
-    ith_key = list_index[i]
-    # print(f'{ith_value} and {ith_key}')
-    df.loc[df['total_category'] == ith_value, 'total_category_name'] =  ith_key
+    ith_key = float(list_index[i])
+    print(type(ith_key))
+    print(f'Dictionary value {ith_value} and key {ith_key}')
+    df.loc[df['total_category'] == ith_key, 'total_category_name'] =  ith_value
 
-print(df.describe())
+print(df.head())
+
+# The first part of the problem was to replace the nulls with "Unknown"
+
+print(df.isna().sum())
+df.loc[df['total_category'].isna(), 'total_category_name'] = 'Unknown'
+print(df.head())
+
+# Second part of the problem was to find the average runtime (here we will do speed) 
+# for each total_category mimited to one value of another column
+
